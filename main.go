744a138377d1cae62831b1b2ee974ed1ba09d019
sthsuyash <sthasuyash11@gmail.com>
2023-09-23 21:47:07 +0545

    middleware chi

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http" // this package is used to create a web server
-	
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	// fmt.Println("Hello, World!") // Println is a camel case function where first letter is capital because it is public
 	// creating a server object
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
-		Addr:    ":8080",                        // port number
-		Handler: http.HandlerFunc(basicHandler), // handler function
+		Addr: ":8080", // port number
+		// Handler: http.HandlerFunc(basicHandler), // handler function // this is for core go
+		Handler: router, // this is for chi router
 	}
 
 	err := server.ListenAndServe() // ListenAndServe is a blocking function
@@ -22,16 +30,20 @@ func main() {
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World from /hello home api!")) // Write function takes a byte array as input
 
-	if r.Method == http.MethodGet {
-		// handle GET
-		if r.URL.Path == "/" {
-			w.Write([]byte("Hello, World from home api!")) // Write function takes a byte array as input
-			return
-		}
-		if r.URL.Path == "/about" {
-			w.Write([]byte("Hello, World from about api!"))
-			return
-		}
-	}
+	/*
+		This is for core go
+	*/
+	// if r.Method == http.MethodGet {
+	// 	// handle GET
+	// 	if r.URL.Path == "/" {
+	// 		w.Write([]byte("Hello, World from home api!")) // Write function takes a byte array as input
+	// 		return
+	// 	}
+	// 	if r.URL.Path == "/about" {
+	// 		w.Write([]byte("Hello, World from about api!"))
+	// 		return
+	// 	}
+	// }
 }
